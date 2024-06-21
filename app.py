7c52a8f4428b62ec64814ec7071948194b16c8d5
fsdfsdfsd51 <ellirubimac@gmail.com>
2024-06-21 18:23:38 -0400

    Update app.py
    TEMPORARY takes only two inputs

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def random_redirect():
-    return redirect(random.choice(["https://docs.google.com/forms/d/e/1FAIpQLSerCMQbpODwjCZn-47LczWw2nZyNmuhfgFn26T6WjQ1VJf4yg/viewform?usp=sf_link", "https://docs.google.com/forms/d/e/1FAIpQLSfZcyp5Gva0GvZmluhlSN2T8waO5opstlBC3D2i3uFnvMmJMw/viewform?usp=sf_link", "https://docs.google.com/forms/d/e/1FAIpQLSdywQ7eIMRIHwd6qC4NMpbHjaZ4UYpw82xTSUbJ7LlYI2RPsQ/viewform?usp=sf_link", "https://docs.google.com/forms/d/e/1FAIpQLSe1mj2eeao33uiij-ZGE2dwpPsp6SYywZN5Rs44L8a58PR8fg/viewform?usp=sf_link"]))
+    return redirect(random.choice(["https://docs.google.com/forms/d/e/1FAIpQLSerCMQbpODwjCZn-47LczWw2nZyNmuhfgFn26T6WjQ1VJf4yg/viewform?usp=sf_link", "https://docs.google.com/forms/d/e/1FAIpQLSfZcyp5Gva0GvZmluhlSN2T8waO5opstlBC3D2i3uFnvMmJMw/viewform?usp=sf_link"]))
+#    return redirect(random.choice(["https://docs.google.com/forms/d/e/1FAIpQLSerCMQbpODwjCZn-47LczWw2nZyNmuhfgFn26T6WjQ1VJf4yg/viewform?usp=sf_link", "https://docs.google.com/forms/d/e/1FAIpQLSfZcyp5Gva0GvZmluhlSN2T8waO5opstlBC3D2i3uFnvMmJMw/viewform?usp=sf_link", "https://docs.google.com/forms/d/e/1FAIpQLSdywQ7eIMRIHwd6qC4NMpbHjaZ4UYpw82xTSUbJ7LlYI2RPsQ/viewform?usp=sf_link", "https://docs.google.com/forms/d/e/1FAIpQLSe1mj2eeao33uiij-ZGE2dwpPsp6SYywZN5Rs44L8a58PR8fg/viewform?usp=sf_link"]))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
