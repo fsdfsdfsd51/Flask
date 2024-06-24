fe741ea1d4f93e85d2dfbf586863a53a626d7b6c
fsdfsdfsd51 <ellirubimac@gmail.com>
2024-06-24 17:31:29 -0400

    Update app.py
    Temp, HO only

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def random_redirect():
-    return redirect("https://docs.google.com/forms/d/e/1FAIpQLSdywQ7eIMRIHwd6qC4NMpbHjaZ4UYpw82xTSUbJ7LlYI2RPsQ/viewform?usp=sf_link")
+    return redirect("https://docs.google.com/forms/d/e/1FAIpQLSerCMQbpODwjCZn-47LczWw2nZyNmuhfgFn26T6WjQ1VJf4yg/viewform?usp=sf_link")
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
