64f61e38853646b2242101a19425c8a17bf0423a
fsdfsdfsd51 <ellirubimac@gmail.com>
2024-06-24 15:42:16 -0400

    Update app.py
    Temporary high agreeableness tests

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def random_redirect():
-    return redirect("https://docs.google.com/forms/d/e/1FAIpQLSe1mj2eeao33uiij-ZGE2dwpPsp6SYywZN5Rs44L8a58PR8fg/viewform?usp=sf_link")
+    return redirect("https://docs.google.com/forms/d/e/1FAIpQLSdywQ7eIMRIHwd6qC4NMpbHjaZ4UYpw82xTSUbJ7LlYI2RPsQ/viewform?usp=sf_link")
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
